473def6d007e8c983bb0ab42eb247235d409cc20
metecicek209-a11y <metecicek209@gmail.com>
2025-11-08 11:14:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ document.getElementById("convertButton").onclick = function(){
 	let pound = (kgInput * 2.20).toFixed(2);
 	document.getElementById("kgOut").innerHTML = "Pound : " + pound;
 	document.getElementById("comment1").style.visibility = "visible";
+	if(isNaN(check)){
+		document.getElementById("comment2").innerHTML = "Wrong Type";
+		document.getElementById("box1height").style.background = "rgba(255, 0, 0, 0.5)";
+	}
+	else{
+		document.getElementById("box1weight").style.background = "rgba(255, 255, 255, 0.25);";
+	}
 	if(kgInput > 100){
 		document.getElementById("comment1").innerHTML = "Heavy";
 	}
@@ -15,16 +22,19 @@ document.getElementById("convertButton").onclick = function(){
 	}
 	let check = Number(kgInput);
 
-	if(isNaN(check)){
-		document.getElementById("comment1").innerHTML = "Wrong Type";
-		document.getElementById("box1weight").style.background = "rgba(255, 0, 0, 0.5)";
-	}
 }
 document.getElementById("convertButton2").onclick= function(){
 	let heightInput = document.getElementById("heightInput").value;
 	let feet = (heightInput * 3.28).toFixed(2);
 	document.getElementById("heightOut").innerHTML = "Feet : " + feet;
 	document.getElementById("comment2").style.visibility = "visible";
+	if(isNaN(check)){
+		document.getElementById("comment2").innerHTML = "Wrong Type";
+		document.getElementById("box1height").style.background = "rgba(255, 0, 0, 0.5)";
+	}
+	else{
+		document.getElementById("box1height").style.background = "rgba(255, 255, 255, 0.25);";
+	}
 	if(heightInput > 100){
 		document.getElementById("comment2").innerHTML = "Long";
 	}
@@ -36,16 +46,20 @@ document.getElementById("convertButton2").onclick= function(){
 	}
 	let check = Number(heightInput);
 
-	if(isNaN(check)){
-		document.getElementById("comment2").innerHTML = "Wrong Type";
-		document.getElementById("box1height").style.background = "rgba(255, 0, 0, 0.5)";
-	}
+
 }
 document.getElementById("convertButton3").onclick= function(){
 	let tempInput = document.getElementById("tempInput").value;
 	let fahrenheit = ((tempInput * (9/5)) + 32).toFixed(2);
 	document.getElementById("tempOut").innerHTML = "Fahrenheit : " + fahrenheit;
 	document.getElementById("comment3").style.visibility = "visible";
+	if(isNaN(check)){
+		document.getElementById("comment3").innerHTML = "Wrong Type";
+		document.getElementById("box1temp").style.background = "rgba(255, 0, 0, 0.5)";
+	}
+	else{
+		document.getElementById("box1temp").style.background = "rgba(255, 255, 255, 0.25);";
+	}
 	if(tempInput > 100){
 		document.getElementById("comment3").innerHTML = "Hot";
 	}
@@ -57,8 +71,7 @@ document.getElementById("convertButton3").onclick= function(){
 	}
 	let check = Number(tempInput);
 
-	if(isNaN(check)){
-		document.getElementById("comment3").innerHTML = "Wrong Type";
-		document.getElementById("box1temp").style.background = "rgba(255, 0, 0, 0.5)";
-	}
+	
+	
+
 }
